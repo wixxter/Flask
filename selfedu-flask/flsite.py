73ecb245a512a29d9c7d6db3d65332151bee0249
wixxter <wixxters@gmail.com>
2022-06-21 18:26:06 +0500

    #1

--- selfedu-flask/flsite.py
+++ selfedu-flask/flsite.py
@@ -1,20 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-
 menu = ['First', 'Second', 'Three']
+user = ['Alpha', 'Beta', 'Gamma']
 
 @app.route('/')
-@app.route('/index')
 def index():
+    print(url_for('index'))
     return render_template('index.html', title='Все Фласк', menu=menu)
 
-@app.route('/about_us')
+
 @app.route('/about')
 def about():
-    return render_template('about.html', title='Про нас')
+    print(url_for('about'))
+    return render_template('about.html', title='Про нас', menu=user)
 
 
-if __name__ =="__main__":
+@app.route('/profile/<username>') #<path:username>asd/123)
+def profile(username):
+    return f'Пользователь: {username}'
+
+
+if __name__ == "__main__":
     app.run(debug=True)
+
+#with app.test_request_context():
+#    print(url_for('index'))
